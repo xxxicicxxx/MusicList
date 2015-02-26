bc15431061f128cdc2765bfecd8b8ea71e3ebe37
xxxicicxxx <icicerror@gmail.com>
2015-02-26 11:06:41 -0500

    MoreYes

--- MusicRunner.java
+++ MusicRunner.java
@@ -12,8 +12,11 @@ public class MusicRunner
     MusicLibrary Songs = new MusicLibrary();
     int count = 0;
     MusicReader mr = new MusicReader();
+    
     mr.open("musiclist.csv");
+    
     String[] data = mr.getSongData();
+    
     // First line contains all the fields - We don't want to save this anywhere but we can
     // print it for now to see what information we have.
     // System.out.println(Arrays.toString(data));
@@ -23,23 +26,23 @@ public class MusicRunner
     // IS song data available
     while (data != null)
   {
-  // You probably will comment this out but for now print out the line so you can see what is there
-  // Let's try to create a Song object
-  Song song = new Song(runninNrunnin(data[0]), runninNrunnin(data[1]), Integer.parseInt(runninNrunnin(data[3])), Double.parseDouble(runninNrunnin(data[4])), data[16]); // data[0] is the artist and data[1] is the name
-  if(runninNrunnin(data[2]).equals("song"))
-  {
-    Songs.add(song);
-    count++;
-  }
-  if (count == 10) // For now only read ONE song
-  break;
-  data = mr.getSongData(); // Get next line of song data
-  }
-for(int i = Songs.size() - 1; i >=0; i--)
-  {
-    Song song = Songs.get(i);
-    System.out.println("Artist: \""+song.artist + "\" Title: \"" + song.name+"\"");
-  }
-  mr.close();
+      // You probably will comment this out but for now print out the line so you can see what is there
+      // Let's try to create a Song object
+      Song song = new Song(runninNrunnin(data[0]), runninNrunnin(data[1]), Integer.parseInt(runninNrunnin(data[3])), Double.parseDouble(runninNrunnin(data[4])), data[16]); // data[0] is the artist and data[1] is the name
+      if(runninNrunnin(data[2]).equals("song"))
+      {
+        Songs.add(song);
+        count++;
+      }
+      if (count == 10) // For now only read ONE song
+      break;
+      data = mr.getSongData(); // Get next line of song data
+      }
+    for(int i = Songs.size() - 1; i >=0; i--)
+      {
+        Song song = Songs.get(i);
+        System.out.println("Artist: \""+song.artist + "\" Title: \"" + song.name+"\"");
+      }
+    mr.close();
   }
 }
