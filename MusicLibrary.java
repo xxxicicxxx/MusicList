f8c89a6204af4c13c8a8dc0d2bfd1487b41536aa
xxxicicxxx <icicerror@gmail.com>
2015-02-26 11:03:56 -0500

    Yes

--- MusicLibrary.java
+++ MusicLibrary.java
@@ -3,52 +3,56 @@ import java.util.*;
 public class MusicLibrary {
   
   private ArrayList<Song> songs;
-  private int currentSortField = 0;  // Unsorted
-  
-  public MusicLibrary() { 
+  private int currentSortField = 0; // Unsorted
+  public MusicLibrary() {
     /* YOUR CONSTRUCTOR CODE HERE*/
+    this.songs = new ArrayList<Song>();
   }
   
-  /* 
-   * Add song to songs ArrayList
-   */
+  /*
+  * Add song to songs ArrayList
+  */
   public void addSong (Song song)
   {
+    songs.add(song);
   }
   
   /*
-   * Retrieve a song by number
-   * Return null if song index num is out of bounds
-   */
+  * Retrieve a song by number
+  * Return null if song index num is out of bounds
+  */
   public Song getSong (int num)
   {
-    return null;
+    if(num >songs.size())
+      return null;
+    else
+      return songs.get(num-1);
   }
   
   /*
-   * Sort song list by title
-   */
+  * Sort song list by title
+  */
   public void Sort ()
   {
-     currentSortField = field;
+  Collections.sort(songs);
   }
   
   /*
-   * Sort song list by a specific field
-   * 
-   * 1 = title
-   * 2 = artist
-   */
+  * Sort song list by a specific field
+  *
+  * 1 = title
+  * 2 = artist
+  */
   public void Sort (int field)
   {
-    currentSortField = field;
+  currentSortField = field;
   }
-  
   /*
-   * Shuffle song list
-   */
+  * Shuffle song list
+  */
   public void Shuffle ()
   {
+    
     long seed = System.nanoTime();
     Collections.shuffle(songs, new Random(seed));
   }
--- MusicRunner.java
+++ MusicRunner.java
@@ -1,41 +1,45 @@
 import java.util.Arrays;
 
+import java.util.*;
+
 public class MusicRunner
-{
+  {
+  public static String runninNrunnin(String input)
+  {
+    return input.substring(1,input.length()-1);}
   public static void main (String[] args)
   {
+    MusicLibrary Songs = new MusicLibrary();
     int count = 0;
     MusicReader mr = new MusicReader();
-    
     mr.open("musiclist.csv");
-    
     String[] data = mr.getSongData();
-    
     // First line contains all the fields - We don't want to save this anywhere but we can
     // print it for now to see what information we have.
-    System.out.println(Arrays.toString(data));
-    
-    data = mr.getSongData();  // Get next line of song data
-    
+    // System.out.println(Arrays.toString(data));
+    data = mr.getSongData(); // Get next line of song data
     // if data is null then we were unable to read a line of song data, so
     // this loop will continue to read lines of song data as long as there
     // IS song data available
     while (data != null)
-    {
-      // You probably will comment this out but for now print out the line so you can see what is there
-      System.out.println(Arrays.toString(data));
-      
-      // Let's try to create a Song object
-      Song song = new Song(data[0], data[1]);  // data[0] is the artist and data[1] is the name
-      
-      count++;
-      
-      if (count == 1)  // For now only read ONE song
-        break;
-      
-      data = mr.getSongData();  // Get next line of song data
-    }
-    
-    mr.close();
+  {
+  // You probably will comment this out but for now print out the line so you can see what is there
+  // Let's try to create a Song object
+  Song song = new Song(runninNrunnin(data[0]), runninNrunnin(data[1]), Integer.parseInt(runninNrunnin(data[3])), Double.parseDouble(runninNrunnin(data[4])), data[16]); // data[0] is the artist and data[1] is the name
+  if(runninNrunnin(data[2]).equals("song"))
+  {
+    Songs.add(song);
+    count++;
+  }
+  if (count == 10) // For now only read ONE song
+  break;
+  data = mr.getSongData(); // Get next line of song data
+  }
+for(int i = Songs.size() - 1; i >=0; i--)
+  {
+    Song song = Songs.get(i);
+    System.out.println("Artist: \""+song.artist + "\" Title: \"" + song.name+"\"");
+  }
+  mr.close();
   }
 }
